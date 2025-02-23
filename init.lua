9b5d1a7bd0b7d1829a28aa42eb13c03d3a62e43b
gdeshazer <6433584+gdeshazer@users.noreply.github.com>
2025-02-23 14:43:40 -0700

    add markdown format setting

--- init.lua
+++ init.lua
@@ -115,6 +115,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('FileType', {
+  desc = 'Markdown format settings',
+  pattern = 'markdown',
+  callback = function()
+    vim.opt_local.textwidth = 120
+    vim.opt_local.colorcolumn = '+1'
+    vim.opt_local.formatoptions:append 't'
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
