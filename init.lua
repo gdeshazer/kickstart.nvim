02ae71fd91f3a9698a6f6d3e4886825ab634289e
gdeshazer <6433584+gdeshazer@users.noreply.github.com>
2026-04-03 09:51:39 -0600

    add vscode split to avoid loading plugins

--- init.lua
+++ init.lua
@@ -8,7 +8,12 @@ vim.g.have_nerd_font = true
 require 'options'
 require 'keymaps'
 require 'lazy-bootstrap'
-require 'lazy-plugins'
+
+if vim.g.vscode then
+  require 'lazy-plugins-vscode'
+else
+  require 'lazy-plugins'
+end
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/lazy-plugins-vscode.lua
@@ -0,0 +1,29 @@
+-- [[ Configure and install plugins ]]
+require('lazy').setup({
+
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.which-key',
+  require 'kickstart.plugins.treesitter',
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
+
+-- vim: ts=2 sts=2 sw=2 et
